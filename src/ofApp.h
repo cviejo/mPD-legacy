0827d92de0506de514e05efa98b6d0280c6f8eea
Carlos Viejo <carlos.viejo.muros@gmail.com>
2016-12-26 14:34:57 +0100

    removed unused stuff

--- src/ofApp.h
+++ src/ofApp.h
@@ -20,15 +20,7 @@ class ofApp : public ofBaseApp {
 		void draw();
 
 		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mousePressed(int x, int y, int button);
-		void mouseDragged(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
 		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
 		
 		void audioReceived (float * aInput,  int aBufferSize, int aChannelCount);
