59c33506fba30e101f337362b442534683570739
carlos <carlos.viejo.muros@gmail.com>
2017-01-12 12:58:52 +0100

    refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -15,19 +15,25 @@ gulp.task('default', function(){
 
 	gulp.watch('./src/**/*.*', function(e) {
 
-		if (make){
-			make.kill();
-			exec("ps -a | grep mPD | cut -f 1 -d \" \" | xargs kill");
-		}
+		killApp();
 
 		make = spawn("make", opts);
 
 		make.on('close', function(err) {
 
 			if (!err && run){
+				killApp();
 				make = spawn("make", ["RunRelease"], opts);
 			}
 		});
 	});
 });
 
+
+function killApp(){
+
+	if (make){
+		make.kill();
+		exec("ps -a | grep mPD | cut -f 1 -d \" \" | xargs kill");
+	}
+}
